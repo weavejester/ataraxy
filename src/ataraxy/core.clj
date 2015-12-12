8b899fd14e98f1cb6b4f29698e1999c23e3803ef
James Reeves <jreeves@weavejester.com>
2015-12-12 15:51:59 +0000

    Add most basic case for matching static paths

--- src/ataraxy/core.clj
+++ src/ataraxy/core.clj
@@ -1,6 +1,16 @@
-(ns ataraxy.core)
+(ns ataraxy.core
+  (:refer-clojure :exclude [compile])
+  (:require [clojure.set :as set]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Routes
+  (matches [routes request])
+  (generate [routes outcome]))
+
+(defn compile [routes]
+  (let [lookup (set/map-invert routes)]
+    (reify Routes
+      (matches [_ request]
+        (routes (:uri request)))
+      (generate [_ outcome]
+        (if-let [uri (lookup outcome)]
+          {:uri uri})))))
--- test/ataraxy/core_test.clj
+++ test/ataraxy/core_test.clj
@@ -1,7 +1,15 @@
 (ns ataraxy.core-test
   (:require [clojure.test :refer :all]
-            [ataraxy.core :refer :all]))
+            [ataraxy.core :as ataraxy]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-matches
+  (let [routes (ataraxy/compile {"/foo" :foo, "/bar" :bar})]
+    (is (= (ataraxy/matches routes {:uri "/foo"}) :foo))
+    (is (= (ataraxy/matches routes {:uri "/bar"}) :bar))
+    (is (nil? (ataraxy/matches routes {:uri "/baz"})))))
+
+(deftest test-generate
+  (let [routes (ataraxy/compile {"/foo" :foo, "/bar" :bar})]
+    (is (= (ataraxy/generate routes :foo) {:uri "/foo"}))
+    (is (= (ataraxy/generate routes :bar) {:uri "/bar"}))
+    (is (nil? (ataraxy/generate routes :baz)))))
