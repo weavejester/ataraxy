a98d808e1738f2e988b262ec332642ffa15dabb7
James Reeves <jreeves@weavejester.com>
2015-12-17 01:16:49 +0000

    Add some blank newlines to split up tests

--- test/ataraxy/core_test.clj
+++ test/ataraxy/core_test.clj
@@ -8,10 +8,12 @@
       (is (= (ataraxy/matches routes {:uri "/foo"}) [:foo]))
       (is (= (ataraxy/matches routes {:uri "/bar"}) [:bar]))
       (is (nil? (ataraxy/matches routes {:uri "/baz"})))))
+
   (testing "compiled routes"
     (let [routes (ataraxy/compile '{"/foo" :foo})]
       (is (= (ataraxy/matches routes {:uri "/foo"}) [:foo]))
       (is (nil? (ataraxy/matches routes {:uri "/bar"})))))
+
   (testing "parameters"
     (let [routes '{["/foo/" x]           [:foo x]
                    ["/foo/" x "/bar/" y] [:foobar x y]}]
@@ -19,10 +21,12 @@
       (is (= (ataraxy/matches routes {:uri "/foo/8/bar/3a"}) [:foobar "8" "3a"]))
       (is (nil? (ataraxy/matches routes {:uri "/foo"})))
       (is (nil? (ataraxy/matches routes {:uri "/foo/44/bar/"})))))
+
   (testing "methods"
     (let [routes '{(:get ["/foo/" id]) [:foo id]}]
       (is (= (ataraxy/matches routes {:request-method :get, :uri "/foo/10"}) [:foo "10"]))
       (is (nil? (ataraxy/matches routes {:request-method :post, :uri "/foo/10"})))))
+
   (testing "request destructuring"
     (let [routes '{(:get ["/search"] {:params {:q q}}) [:search q]}]
       (is (= (ataraxy/matches routes {:request-method :get
@@ -36,20 +40,24 @@
       (is (= (ataraxy/generate routes :foo) {:uri "/foo"}))
       (is (= (ataraxy/generate routes :bar) {:uri "/bar"}))
       (is (nil? (ataraxy/generate routes :baz)))))
+
   (testing "compiled routes"
     (let [routes (ataraxy/compile '{"/foo" :foo})]
       (is (= (ataraxy/generate routes [:foo])  {:uri "/foo"}))
       (is (nil? (ataraxy/generate routes [:bar])))))
+
   (testing "parameters"
     (let [routes '{["/foo/" x]           [:foo x]
                    ["/foo/" x "/bar/" y] [:foobar x y]}]
       (is (= (ataraxy/generate routes [:foo "10"])        {:uri "/foo/10"}))
       (is (= (ataraxy/generate routes [:foobar "8" "3a"]) {:uri "/foo/8/bar/3a"} ))
       (is (nil? (ataraxy/generate routes [:bar])))))
+
   (testing "methods"
     (let [routes '{(:get ["/foo/" id]) [:foo id]}]
       (is (= (ataraxy/generate routes [:foo "10"]) {:request-method :get, :uri "/foo/10"}))
       (is (nil? (ataraxy/generate routes [:bar "10"])))))
+
   (testing "request restructuring"
     (let [routes '{(:get ["/search"] {:params {:q q}}) [:search q]}]
       (is (= (ataraxy/generate routes [:search "foobar"])
