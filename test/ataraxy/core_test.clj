b99366c478df6a14901dd48b33cb57535e74c913
James Reeves <jreeves@weavejester.com>
2015-12-17 03:58:14 +0000

    Make test-matches clearer with 'are' macro

--- test/ataraxy/core_test.clj
+++ test/ataraxy/core_test.clj
@@ -5,39 +5,45 @@
 (deftest test-matches
   (testing "static routes"
     (let [routes '{"/foo" :foo, "/bar" :bar}]
-      (is (= (ataraxy/matches routes {:uri "/foo"}) [:foo]))
-      (is (= (ataraxy/matches routes {:uri "/bar"}) [:bar]))
-      (is (nil? (ataraxy/matches routes {:uri "/baz"})))))
+      (are [req res] (= (ataraxy/matches routes req) res)
+        {:uri "/foo"} [:foo]
+        {:uri "/bar"} [:bar]
+        {:uri "/baz"} nil)))
 
   (testing "compiled routes"
     (let [routes (ataraxy/compile '{"/foo" :foo})]
-      (is (= (ataraxy/matches routes {:uri "/foo"}) [:foo]))
-      (is (nil? (ataraxy/matches routes {:uri "/bar"})))))
+      (are [req res] (= (ataraxy/matches routes req) res)
+        {:uri "/foo"} [:foo]
+        {:uri "/bar"} nil)))
 
   (testing "parameters"
     (let [routes '{["/foo/" x]           [:foo x]
                    ["/foo/" x "/bar/" y] [:foobar x y]}]
-      (is (= (ataraxy/matches routes {:uri "/foo/10"})       [:foo "10"]))
-      (is (= (ataraxy/matches routes {:uri "/foo/8/bar/3a"}) [:foobar "8" "3a"]))
-      (is (nil? (ataraxy/matches routes {:uri "/foo"})))
-      (is (nil? (ataraxy/matches routes {:uri "/foo/44/bar/"})))))
+      (are [req res] (= (ataraxy/matches routes req) res)
+        {:uri "/foo/10"}       [:foo "10"]
+        {:uri "/foo/8/bar/3a"} [:foobar "8" "3a"]
+        {:uri "/foo"}          nil
+        {:uri "/foo/44/bar/"}  nil)))
 
   (testing "methods"
     (let [routes '{(:get ["/foo/" id]) [:foo id]}]
-      (is (= (ataraxy/matches routes {:request-method :get, :uri "/foo/10"}) [:foo "10"]))
-      (is (nil? (ataraxy/matches routes {:request-method :post, :uri "/foo/10"})))))
+      (are [req res] (= (ataraxy/matches routes req) res)
+        {:request-method :get, :uri "/foo/10"}  [:foo "10"]
+        {:request-method :post, :uri "/foo/10"} nil)))
 
   (testing "request destructuring"
-    (let [routes '{(:get ["/search"] {:params {:q q}}) [:search q]}]
-      (is (= (ataraxy/matches routes {:request-method :get
-                                      :uri "/search"
-                                      :params {:q "foobar"}})
-             [:search "foobar"]))))
+    (let [routes '{(:get ["/find"] {:params {:q q}}) [:find q]}]
+      (are [req res] (= (ataraxy/matches routes req) res)
+        {:request-method :get, :uri "/find", :params {:q "x"}}  [:find "x"]
+        {:request-method :get, :uri "/found", :params {:q "x"}} nil)))
 
   (testing "partial routes"
-    (let [routes '{("/foo" {:params {:q q}}) [:foo q]}]
-      (is (= (ataraxy/matches routes {:uri "/foo", :params {:q "x"}})
-             [:foo "x"])))))
+    (let [routes '{("/foo"   {:params {:q q}}) [:foo q]
+                   (["/bar"] {:params {:q q}}) [:bar q]}]
+      (are [req res] (= (ataraxy/matches routes req) res)
+        {:uri "/foo", :params {:q "x"}} [:foo "x"]
+        {:uri "/bar", :params {:q "x"}} [:bar "x"]
+        {:uri "/baz", :params {:q "x"}} nil))))
 
 (deftest test-generate
   (testing "static routes"
