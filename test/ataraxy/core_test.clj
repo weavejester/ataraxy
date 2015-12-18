3d0307750a9f2dd9a094af2c2ef4014db686437a
James Reeves <jreeves@weavejester.com>
2015-12-18 20:39:03 +0000

    Make match tests a little more descriptive

--- test/ataraxy/core_test.clj
+++ test/ataraxy/core_test.clj
@@ -3,7 +3,7 @@
             [ataraxy.core :as ataraxy]))
 
 (deftest test-matches
-  (testing "static routes"
+  (testing "string routes"
     (let [routes '{"/foo" :foo, "/bar" :bar}]
       (are [req res] (= (ataraxy/matches routes req) res)
         {:uri "/foo"} [:foo]
@@ -16,7 +16,7 @@
         {:uri "/foo"} [:foo]
         {:uri "/bar"} nil)))
 
-  (testing "parameters"
+  (testing "vector routes"
     (let [routes '{["/foo/" x]           [:foo x]
                    ["/foo/" x "/bar/" y] [:foobar x y]}]
       (are [req res] (= (ataraxy/matches routes req) res)
@@ -25,7 +25,7 @@
         {:uri "/foo"}          nil
         {:uri "/foo/44/bar/"}  nil)))
 
-  (testing "methods"
+  (testing "list routes with methods"
     (let [routes '{(:get ["/foo/" id])  [:foo id]
                    (method ["/bar/" id]) [:bar method id]}]
       (are [req res] (= (ataraxy/matches routes req) res)
@@ -34,19 +34,24 @@
         {:request-method :post, :uri "/bar/12"} [:bar :post "12"]
         {:request-method :post, :uri "/foo/10"} nil)))
 
-  (testing "request destructuring"
-    (let [routes '{(:get ["/find"] {:params {:q q}}) [:find q]}]
+  (testing "list routes with paths"
+    (let [routes '{(["/foo/" id]) [:foo id]
+                   ("/bar")       [:bar]}]
       (are [req res] (= (ataraxy/matches routes req) res)
-        {:request-method :get, :uri "/find", :params {:q "x"}}  [:find "x"]
-        {:request-method :get, :uri "/found", :params {:q "x"}} nil)))
+        {:uri "/foo/10"}                    [:foo "10"]
+        {:uri "/bar"}                       [:bar]
+        {:request-method :get, :uri "/bar"} [:bar]
+        {:uri "/bar", :params {:q "q"}}     [:bar])))
 
-  (testing "partial routes"
-    (let [routes '{("/foo"   {:params {:q q}}) [:foo q]
-                   (["/bar"] {:params {:q q}}) [:bar q]}]
+  (testing "list routes with request maps"
+    (let [routes '{(:get ["/foo"] {:params {:x x}}) [:foo x]
+                   ("/bar"        {:params {:y y}}) [:bar y]
+                   (["/baz/" x]   {:params {:z z}}) [:baz x z]}]
       (are [req res] (= (ataraxy/matches routes req) res)
-        {:uri "/foo", :params {:q "x"}} [:foo "x"]
-        {:uri "/bar", :params {:q "x"}} [:bar "x"]
-        {:uri "/baz", :params {:q "x"}} nil))))
+        {:request-method :get, :uri "/foo", :params {:x "x"}} [:foo "x"]
+        {:uri "/bar", :params {:y "y"}}   [:bar "y"]
+        {:uri "/baz/4", :params {:z "z"}} [:baz "4" "z"]
+        {:uri "/foo", :params {:x "x"}}   nil))))
 
 (deftest test-generate
   (testing "static routes"
