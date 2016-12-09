eed7c79c7b07500160121b88ea246031108d89d7
James Reeves <jreeves@weavejester.com>
2016-12-09 11:50:17 +0000

    Reimplement compile and matches functions

--- src/ataraxy/core.clj
+++ src/ataraxy/core.clj
@@ -1,15 +1,18 @@
 (ns ataraxy.core
-  (:require [clojure.spec :as s]
+  (:refer-clojure :exclude [compile])
+  (:require [clojure.set :as set]
+            [clojure.spec :as s]
+            [clojure.string :as str]
             [clojure.core.match :refer [match]]))
 
 (s/def ::route-set
   (s/and set? (s/coll-of symbol?)))
 
 (s/def ::route-single
-  (s/or :method  keyword?
-        :path    (s/or :string string? :symbol symbol?)
-        :params  (s/and set? (s/coll-of symbol?))
-        :request :clojure.core.specs/map-binding-form))
+  (s/or :method   keyword?
+        :path     (s/or :string string? :symbol symbol?)
+        :params   (s/and set? (s/coll-of symbol?))
+        :destruct :clojure.core.specs/map-binding-form))
 
 (s/def ::route-multiple
   (s/and vector? (s/coll-of ::route-single)))
@@ -58,3 +61,78 @@
 (defn parse [routes]
   {:pre [(valid? routes)]}
   (parse-routing-table {} (s/conform ::routing-table routes)))
+
+(defn- compile-match-result [{:keys [key args]}]
+  `[~key ~@args])
+
+(defn- find-symbols [x]
+  (cond
+    (coll? x)   (mapcat find-symbols x)
+    (symbol? x) (list x)))
+
+(defn- compile-match-destruct [request destructs next-form]
+  (if (some? destructs)
+    `(let [~@(mapcat #(vector % request) destructs)]
+       (if (and ~@(find-symbols destructs))
+         ~next-form))
+    next-form))
+
+(defn- compile-match-params [request params next-form]
+  (if (some? params)
+    (let [params (apply set/union params)]
+      `(let [{:keys [~@params]} ~request]
+         (if (and ~@params)
+           ~next-form)))
+    next-form))
+
+(defn- path-symbols [path]
+  (into '[_] (comp (map second) (filter symbol?)) path))
+
+(defn- path-part-regex [[_ part]]
+  (if (string? part)
+    (java.util.regex.Pattern/quote part)
+    (str "(" (:re (meta part) "[^/]+") ")")))
+
+(defn- path-regex [path]
+  (re-pattern (str/join (map path-part-regex path))))
+
+(defn- compile-match-path [request path next-form]
+  (if (some? path)
+    `(let [path-info# (or (:path-info ~request) (:uri ~request))]
+       (if-let [~(path-symbols path) (re-matches ~(path-regex path) path-info#)]
+         ~next-form))
+    next-form))
+
+(defn- compile-match-method [request method next-form]
+  (if (some? method)
+    `(if (= ~(first method) (:request-method ~request)) ~next-form)
+    next-form))
+
+(defn- compile-match-route [request [{:keys [method path params destruct]} result]]
+  (->> (compile-match-result result)
+       (compile-match-destruct request destruct)
+       (compile-match-params request params)
+       (compile-match-path request path)
+       (compile-match-method request method)))
+
+(defn compile-match [routes]
+  (let [request (gensym "request")]
+    `(fn [~request]
+       (or ~@(map (partial compile-match-route request) (parse routes))))))
+
+(defprotocol Routes
+  (-matches [routes request]))
+
+(defmacro compile* [routes]
+  {:pre [(valid? routes)]}
+  `(let [matches# ~(compile-match routes)]
+     (reify Routes
+       (-matches [_ request#] (matches# request#)))))
+
+(defn compile [routes]
+  (eval `(compile* ~routes)))
+
+(defn matches [routes request]
+  (if (satisfies? Routes routes)
+    (-matches routes request)
+    (-matches (compile routes) request)))
