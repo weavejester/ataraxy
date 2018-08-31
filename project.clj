a52acd586410b385d90e3a33dc7e426b8a7f18e1
Scott Lowe <scottwithtwotees@gmail.com>
2018-08-31 15:35:11 -0400

    Bump Clojure dep to the stable 1.9.0 release

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/ataraxy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-core "1.6.1"]]
   :profiles
   {:dev {:dependencies [[criterium "0.4.3"]]}})
